72eabfb551f034869945cf5226049add398962a0
Jocephuss <rowlandjosiah14@gmail.com>
2024-09-06 17:35:48 -0600

    "User.js schema created"

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+// Define the User schema and model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [/.+@.+\..+/, "Please enter a valid email address"], // Regular expression for email validation
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+// Add a virtual property to count the number of friends a user has
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+// Create the User model from the schema
+const User = model("User", userSchema);
+module.exports = User;
